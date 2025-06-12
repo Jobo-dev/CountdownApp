103eca18c21ee1f8dd35fc25d5513fb035b84e02
CJ <jorgecj.dev@gmail.com>
2025-06-11 22:43:18 -0600

    Modify the logic on the settings screen main script to get the info now from the dropdowns

--- Assets/ProjectAssets/Scripts/CountdownSettings.cs
+++ Assets/ProjectAssets/Scripts/CountdownSettings.cs
@@ -1,18 +1,35 @@
+using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CountdownSettings : CountdownElement
 {
-    [SerializeField] TMP_InputField targetDateIF;
+    
+    [SerializeField] List<TMP_Dropdown> targetDropDownList;
+    [SerializeField] List<TMP_Dropdown> targetHourDropDownList;
+    //[SerializeField] List<TMP_Dropdown> initialDropDownList;
+
+
     [SerializeField] TMP_InputField descriptionIF;
     [SerializeField] Button startButton;
     [SerializeField] Button closeButton;
 
     private void Awake()
     {
+        if (targetDropDownList.Count == 0)
+        {
+            Debug.LogWarning($"{GetType()} Warning. The target date dropdown list is empty, please assign the corresponding elements");
+            return;
+        }
+        if (targetHourDropDownList.Count == 0)
+        {
+            Debug.LogWarning($"{GetType()} Warning. The target hour dropdown list is empty, please assign the corresponding elements");
+            return;
+        }
+
         elementId = ElementId.SettingsScreen;
-        targetDateIF.text = "";
         startButton.onClick.AddListener(ValidateSettings);
         closeButton.onClick.AddListener(() =>
         {
@@ -60,7 +77,7 @@ public class CountdownSettings : CountdownElement
 
     private bool ValidateDate()
     {
-        string dateText = targetDateIF.text;
+        string dateText = SetDateTimeAsString(false);
 
         if (dateText != "")
         {
@@ -81,6 +98,34 @@ public class CountdownSettings : CountdownElement
             return false;
         }
             
+    }
+
+    string BuildDateTimeString(string separator, List<TMP_Dropdown> dropdownList)
+    {
+        List<string> dataList = new List<string>();
+
+        foreach (var dropdown in dropdownList)
+        {
+            dataList.Add(dropdown.options[dropdown.value].text);
+        }
+
+        string dateText = string.Join(separator, dataList);
+        return dateText;
+    }
+
+    string SetDateTimeAsString(bool setMidnight)
+    {
+        string hour = "";
+        
+        if (setMidnight)
+            hour = "00:00:00";
+        else
+            hour = BuildDateTimeString(":", targetHourDropDownList);        
+        
+        string date = BuildDateTimeString("-", targetDropDownList);
+        
+        string[] dateTime = { date, hour };
+        return string.Join(" ", dateTime);
     }
 
     internal override void InitElement()
